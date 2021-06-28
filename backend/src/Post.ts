e6f8582c8612c52c535f7f16303b64078bf515b0
Mark Sy <marksy@jaunthealthcare.com>
2021-06-27 22:05:47 -0400

    Post ObjectType + PostResolver getPostById, incrementListCount, deletePostById

---
+++ backend/src/Post.ts
@@ -0,0 +1,27 @@
+import 'reflect-metadata'
+import { ObjectType, Field, ID, Int } from 'type-graphql'
+import { User } from './User'
+
+@ObjectType()
+export class Post {
+  @Field((type) => ID)
+  id: number
+
+  @Field((type) => Int, { nullable: true })
+  likeCount: number | null
+  
+  @Field((type) => Int, { nullable: true })
+  commentCount: number | null
+
+  @Field((type) => String, { nullable: true })
+  postType: string | null
+
+  @Field((type) => String, { nullable: true })
+  mediaURL: string | null
+
+  @Field((type) => Boolean, { nullable: true })
+  published?: boolean | null
+
+  @Field((type) => User, { nullable: true })
+  author?: User | null
+}
---
+++ backend/src/PostResolver.ts
@@ -0,0 +1,63 @@
+import 'reflect-metadata'
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  Ctx,
+
+  Int,
+  InputType,
+  Field,
+} from 'type-graphql'
+import { Post } from './Post'
+import { Context } from './Context'
+
+@InputType()
+export class PostCreateInput {
+  @Field({ nullable: true })
+  likeCount: number
+
+  @Field({ nullable: true })
+  postType: string
+}
+
+export enum SortOrder {
+  asc = 'asc',
+  desc = 'desc'
+}
+
+@Resolver(Post)
+export class PostResolver {
+
+  @Query((returns) => Post, { nullable: true })
+  async postById(@Arg('id') id: number, @Ctx() ctx: Context) {
+    return ctx.prisma.post.findFirst({
+      where: { id },
+    })
+  }
+
+  @Mutation((returns) => Post, { nullable: true })
+  async incrementLikeCount(@Arg('id', (type) => Int) id: number, @Ctx() ctx: Context) {
+    return ctx.prisma.post.update({
+      where: { id: id || undefined },
+      data: {
+        likeCount: {
+          increment: 1,
+        },
+      },
+    })
+  }
+
+  @Mutation((returns) => Post, { nullable: true })
+  async deletePost(
+    @Arg('id', (type) => Int) id: number,
+    @Ctx() ctx: Context,
+  ) {
+    return ctx.prisma.post.delete({
+      where: {
+        id
+      },
+    })
+  }
+}
