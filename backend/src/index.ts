e1d853ab2a1ebf40eae71e071d9b9aec5e17d26b
Mark Sy <mark.jesse.sy@gmail.com>
2021-06-27 23:16:48 -0400

    Using apollo-express-server to route graphql service

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,11 @@ import 'reflect-metadata'
 import * as tq from 'type-graphql'
 import { PostCreateInput, PostResolver, SortOrder } from './resolvers/PostResolver'
 import { UserResolver } from './resolvers/UserResolver'
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
 import { DateTimeResolver } from 'graphql-scalars'
 import { context } from './singleton/Context'
 import { GraphQLScalarType } from 'graphql'
-
+import express from 'express'
 
 const app = async () => {
   tq.registerEnumType(SortOrder, {
@@ -18,9 +18,16 @@ const app = async () => {
     scalarsMap: [{ type: GraphQLScalarType, scalar: DateTimeResolver }]
   })
 
-  new ApolloServer({ schema, context: context }).listen({ port: 4000 }, () =>
-    console.log(`Server ready at: http://localhost:4000`)
-  )
+  const server = new ApolloServer({ schema, context: context})
+  await server.start();
+  
+  const app = express();
+  server.applyMiddleware({ app });
+
+  await new Promise(resolve => app.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  
+
 }
 
 app()
