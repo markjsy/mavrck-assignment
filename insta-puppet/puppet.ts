270190f865262efe99e7cfd70605b82c868ab2aa
Mark Sy <mark.jesse.sy@gmail.com>
2021-06-28 22:48:18 -0400

    adding typescript to puppet project

---
+++ insta-puppet/puppet.ts
@@ -0,0 +1,21 @@
+import * as puppeteer from 'puppeteer'
+
+export async function getUserInformation(user) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setExtraHTTPHeaders({
+      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+      'upgrade-insecure-requests': '1',
+      'accept-encoding': 'gzip, deflate, br',
+      'accept-language': 'en-US,en;q=0.9,en;q=0.8'
+    })
+  
+    await page.goto(`https://instagram.com/${user}/channel/?__a=1`);
+    const element = await page.$("pre");
+    const text = await page.evaluate(element => element.textContent, element);
+    console.log(text)
+    await browser.close();
+}
+
+getUserInformation('mavrck')
