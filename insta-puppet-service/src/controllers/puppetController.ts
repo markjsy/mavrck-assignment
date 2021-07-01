e9ecd445327d601d015b351c194e168f6389df0b
Mark Sy <marksy@jaunthealthcare.com>
2021-07-01 11:59:58 -0400

    Added logging

--- insta-puppet-service/src/controllers/puppetController.ts
+++ insta-puppet-service/src/controllers/puppetController.ts
@@ -9,17 +9,15 @@ const addUserRabbitMQ = async (req: Request, res: Response, next: NextFunction) 
     try {
         const connection = await amqplib.connect(RABBIT_AMQP_URL);
         const channel = await connection.createChannel();
-        console.log("Body")
-        console.log(req.body)
         channel.sendToQueue('puppet', Buffer.from(JSON.stringify(req.body)) );
-        console.log("Sent to queue: " + Buffer.from(JSON.stringify(req.body)) );
-        
-    } catch (ex) {
-        console.error('An error occurred when connecting');
-    }
 
+        logging.info("Body of message sent to puppet service: ", req.body)
+        logging.info(NAMESPACE, "Sent to queue: " + Buffer.from(JSON.stringify(req.body)) );       
+    } catch (ex) {
+        logging.error(NAMESPACE, 'An error occurred when connecting');
+    }
     return res.status(200).json({
-        message: 'rabbit mq service'
+        message: 'Successfully added a new user'
     });
 };
 
