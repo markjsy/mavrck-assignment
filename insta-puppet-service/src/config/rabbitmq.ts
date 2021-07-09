6fc33161ebf37db131476b827be16b5c29e6258d
Mark Sy <marksy@jaunthealthcare.com>
2021-07-08 20:03:54 -0400

    Removing config files

--- insta-puppet-service/src/config/rabbitmq.ts
+++
@@ -1,3 +0,0 @@
-export const RABBIT_AMQP_URL = process.env.AMQP_HOST ? `amqp://${process.env.AMQP_HOST}` : 'amqp://0.0.0.0:5672';
-export const QUEUE_NAME_ADD = process.env.QUEUE_NAME_ADD? process.env.QUEUE_NAME_ADD : 'puppet';
-export const QUEUE_NAME_UPDATE = process.env.QUEUE_NAME_UPDATE? process.env.QUEUE_NAME_UPDATE : 'puppetUpdate';
--- react-redux-ui/src/constants/requests/httpMethodsConstants.ts
+++
@@ -1,9 +0,0 @@
-export const GRAPHQL_URL = process.env.GRAPHQL_URL ? process.env.GRAPHQL_URL : 'http://localhost:2500/graphql';
-
-export const GRAPHQL_SUB_URL = process.env.GRAPHQL_SUB_URL ? process.env.GRAPHQL_SUB_URL : 'ws://localhost:2500/graphql';
-
-export const PUPPET_URL = process.env.PUPPET_URL ? process.env.PUPPET_URL : 'http://localhost:2000/puppet/user';
-
-export const PUPPET_URL_UPDATE = process.env.PUPPET_URL_UPDATE ? process.env.PUPPET_URL_UPDATE : 'http://localhost:2000/puppet/userUpdate';
-
-export const INSTAGRAM_URL = 'https://www.instagram.com';
