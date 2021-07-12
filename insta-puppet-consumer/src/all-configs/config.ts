22393e4c2d9ab08ff424ec5c0be525d6bd6e589c
markjsy <mark.jesse.sy@gmail.com>
2021-07-12 13:42:49 -0400

    env

--- insta-puppet-consumer/src/all-configs/config.ts
+++ insta-puppet-consumer/src/all-configs/config.ts
@@ -4,7 +4,7 @@ const MAIN_HOSTNAME: string  = process.env.MAIN_HOSTNAME ? process.env.MAIN_HOST
 
 export const CONFIG = {
     INSTA_PUPPET_CONSUMER: {
-        RABBIT_AMQP_URL: process.env.AMQP_HOST ? `${process.env.AMQP_HOST}` : `amqp://guest:guest@${MAIN_HOSTNAME}:5672/`,
+        RABBIT_AMQP_URL: process.env.AMQP_HOST ? `${process.env.AMQP_HOST}` : `amqp://guest:guest@localhost:5672/`,
         QUEUE_NAME_ADD: process.env.QUEUE_NAME_ADD ? process.env.QUEUE_NAME_ADD : `puppet`,
         QUEUE_NAME_UPDATE: process.env.QUEUE_NAME_UPDATE ? process.env.QUEUE_NAME_UPDATE : `puppetUpdate`,
         GRAPHQL_URL: process.env.GRAPHQL_URL ? process.env.GRAPHQL_URL : `http://${MAIN_HOSTNAME}:2500/graphql`
--- insta-puppet-service/src/all-configs/config.ts
+++ insta-puppet-service/src/all-configs/config.ts
@@ -5,7 +5,7 @@ export const CONFIG = {
     INSTA_PUPPET_SERVICE: {
         IPS_PORT: process.env.IPS_PORT || 2000,
         HOSTNAME: process.env.SERVER_HOSTNAME || `rabbitmq`,
-        RABBIT_AMQP_URL: process.env.AMQP_HOST ? `${process.env.AMQP_HOST}` : `amqp://guest:guest@${MAIN_HOSTNAME}:5672/`,
+        RABBIT_AMQP_URL: process.env.AMQP_HOST ? `${process.env.AMQP_HOST}` : `amqp://guest:guest@localhost:5672/`,
         QUEUE_NAME_ADD: process.env.QUEUE_NAME_ADD ? process.env.QUEUE_NAME_ADD : `puppet`,
         QUEUE_NAME_UPDATE: process.env.QUEUE_NAME_UPDATE ? process.env.QUEUE_NAME_UPDATE : `puppetUpdate`
     }
